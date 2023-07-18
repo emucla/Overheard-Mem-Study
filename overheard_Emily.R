583bf018a6b9c0b778be2418a9534b3cf8ed6bc7
Emily Neer <100445257+emucla@users.noreply.github.com>
2023-07-18 11:50:53 -0700

    Add files via upload

--- overheard_Emily.R
+++ overheard_Emily.R
@@ -11,12 +11,16 @@ library(jtools)
 
 #read in the data 
 ovdat <- read.table('video_coding_data.txt', header=T)
+
+#check data frame
+summary(ovdat)
+
+#change response variable to numeric
 ovdat$response <- as.numeric(as.character(ovdat$response))
 
-#copy over data imputation 
+#data imputation code (https://stefvanbuuren.name/fimd/sec-pmm.html)
 imp <- mice(ovdat, method = "pmm", m = 20, maxit = 1)
 ovdat <- complete(imp)
-head(ovdat)
 
 #create function for standard error
 se <- function(x){
@@ -26,35 +30,82 @@ se <- function(x){
 #create a proportion of tar_duration/trial time 
 prop <- ovdat$tar_dur/ovdat$trial_time 
 
-#adding prop value to dataframe: use this dataframe for the analyses 
+#adding prop value to dataframe 
 ovdat2 <- ovdat %>%
   add_column(prop)
 
-#descriptive data 
-
+#descriptive data of attention to target object averaged by subject 
 tar <- tapply(ovdat2$prop, ovdat2$sub_id, mean)
 
+#descriptive data of children's reponses in task averaged by subject
 response <- tapply(ovdat2$response, ovdat2$sub_id, mean)
 
+#new data frame grouping by subid - use this data frame for analyses 
 df2 <- ovdat2 %>% group_by(sub_id, cond) %>% 
-  summarise(attn=mean(prop), score = mean(response), 
+  summarise(attn=mean(prop), score = sum(response), 
             .groups = 'drop') %>%
   as.data.frame()
+#telling R to recognize sub_id as a character so that you can group by it
 df2$sub_id <- as.character(as.numeric(df2$sub_id))
 
-#Question 1: Correlation (target attention and correct response) 
+#Preliminary analysis:  independent samples t-tests with unequal variances because of different sample sizes
+#Are there differences between children's correct responses based on learning schedule? 
+
+#correct responses mean for each condition (descriptive)
+m_res <- tapply(df2$score, df2$cond, mean)
+
+t.test(df2$score ~ df2$cond, var.equal = FALSE)
+
+#Are there differences between attention in the massed and spaced condition?
+
+#mean of attention to target for each condition 
+m_attn <- tapply(df2$attn, df2$cond, mean)
+
+#running t test for attention 
+t.test(df2$attn ~ df2$cond, var.equal = FALSE)
+
+#plot massed and spaced comparison boxplot (overlay indv. data points) 
+df2 %>% 
+  ggplot(aes(x= cond, y= attn, color = cond)) +
+  geom_boxplot()+
+  geom_dotplot(binaxis='y', stackdir='center', dotsize=0.6, position = "jitter")+ 
+  #this chunk of code inputs mean and se bar in boxplot
+  stat_summary(fun.data=mean_se, fun.args = list(mult = 1),
+               geom="pointrange", color="black",
+               shape = 18, size = 1,
+               position = position_dodge(width = 0.9))+
+  #changes the scale of your plot
+  coord_fixed(ratio =4/3)  +
+  #y-axis scale 
+  ylim(0, 1) +
+  labs(x = "Learning Schedule", y = "Attention to Target (Out of 8 Trials)")+
+  theme_classic()+
+  theme(text = element_text(face = "bold", size = 12)) +
+  theme(legend.position = "none")+
+  theme(axis.text.x = element_text(angle = 0, vjust = 1, hjust=.5,face = "bold", size = 12, 
+                                   color = "black"))
+
+
+
+#Question: Does attention to target predict (or relate) to children’s correct responses on task regardless of learning schedule? 
 
 #run correlation test
-cor.test(df2$attn, df2$score, method = "pearson")
+cor.test(df2$attn, df2$score, alternative = "greater", method = "pearson")
 
-#plot scatterplot with trendline - below is example code
+#regression test if we are going to claim directionality 
+regress <- lm(formula = score ~ attn, data = df2) #runs the regression
+summary(regress) #spits out statistics 
+
+#plot scatterplot with trendline
 df2 %>% 
   ggplot(aes(x= attn, y= score)) +
   geom_point(size = 3)+
-  coord_fixed(ratio =2.5/4)  +
+  coord_fixed(ratio =1/8)  +
   scale_x_continuous(limits=c(0, 1))+
-  scale_y_continuous (limits=c(0, 1))+
-  labs(x = "Attention Duration (proportion)", y = "Proportion of Correct Responses")+
+  scale_y_continuous (limits=c(0, 8))+
+  #chance line
+  geom_hline(yintercept=2,linetype="dotted")+
+  labs(x = "Attention Duration (proportion)", y = "Correct Responses (Out of 8 Trials)")+
   geom_smooth(method=lm,se=FALSE,fullrange=TRUE,
               )+
   theme_classic()+
@@ -64,47 +115,9 @@ df2 %>%
   theme(axis.text.x = element_text(angle = 0, vjust = 0.5, hjust=.5,face = "bold", size = 16, 
                                    color = "black")) 
 
-#Question 2: T-test (massed and spaced attention) - run t-test for unequal sample sizes 
-
-#correct responses mean for each condition (descriptive)
-m_res <- tapply(df2$score, df2$cond, mean)
-
-#mean of attention to target for each condition 
-m_attn <- tapply(df2$attn, df2$cond, mean)
-
-#code for t-test comparing the means of the two conditions
-
-#running t test 
-t.test(df2$attn ~ df2$cond, var.equal = FALSE)
-
-#plot massed and spaced comparison boxplot (overlay indv. data points) - below is example code
-
-df2 %>% 
-  ggplot(aes(x= cond, y= attn)) +
-  geom_boxplot()+
-  geom_dotplot(binaxis='y', stackdir='center', dotsize=0.6, position = "jitter")+ 
-  stat_summary(fun.data=mean_se, fun.args = list(mult = 1),
-               geom="pointrange", color="black",
-               shape = 18, size = 1,
-               position = position_dodge(width = 0.9))+
-  #geom_point(aes(fill=cond), colour = "white",pch=21, position=position_dodge(width=0.9)) +
-  coord_fixed(ratio =4/3)  +
-  ylim(0, 1) +
-  labs(x = "Learning Schedule", y = "Attention to Target (Proportion)")+
-  theme_classic()+
-  theme(text = element_text(face = "bold", size = 12)) +
-  theme(legend.position = "none")+
-  theme(axis.text.x = element_text(angle = 0, vjust = 1, hjust=.5,face = "bold", size = 12, 
-                                   color = "black"))
-
-#Question 3: regression analysis (target attention and correct response) - currently does not give a complete output because of fake dataset values
-#link to simple linear regression in R: http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
-regress <- lm(formula = score ~ attn, data = df2)
-summary(regress)
-
-
-#exploratory analyses 
+#Exploratory Analyses 
 #attention between target and distractor (t-test)
 #looking more at distractor are they more likely to pick the distractor object? 
-#age effects on attention 
+#create column where the distractor choice is coded as 1 and everything else is 0 
+ 
 
